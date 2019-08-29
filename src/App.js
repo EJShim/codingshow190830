321addb5800c3ffb57a245ef878f6b221d785972
EJShim <soulrommel@gmail.com>
2019-08-29 11:10:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props){
+    super(props)
+
+
+    this.mouseDown = false;
+    this.state={
+      dividerLeft:50,
+      dividerTop:50,
+    }
+  }
+
+
   onImportMesh(e){
     alert("Import Mesh")
   }
@@ -11,7 +23,73 @@ class App extends Component {
     alert("Import Volume")
   }
 
+  onMouseDown(e){
+    e.preventDefault();
+    this.mouseDown = true;
+  }
+
+  onMouseMove(e){
+    e.preventDefault();
+    if(this.mouseDown){
+      const container = this.refs["rendererContainer"]; 
+      const rect = container.getBoundingClientRect();
+      let x = e.clientX - rect.left;
+      let y = e.clientY - rect.top;
+
+      let updateLeft = x * 100 / container.clientWidth;
+      let updateTop = y * 100 / container.clientHeight;
+      
+      this.setState({
+        dividerLeft:updateLeft,
+        dividerTop:updateTop,
+      })
+
+    }
+  }
+
+  onMouseUp(e){
+    e.preventDefault();
+    this.mouseDown = false;
+  }
+
   render(){
+
+
+    const dividerHStyle={
+      top:`${this.state.dividerTop}%`
+    }
+
+    const dividerVStyle={
+      left:`${this.state.dividerLeft}%`
+    }
+
+    const renderer1Style={
+      width:`${this.state.dividerLeft}%`,
+      height:`${this.state.dividerTop}%`,
+
+    }
+
+    const renderer2Style={
+      left:`${this.state.dividerLeft}%`,
+      width:`${100 - this.state.dividerLeft}%`,
+      height:`${this.state.dividerTop}%`,
+    }
+
+    const renderer3Style={
+      top:`${this.state.dividerTop}%`,
+      height:`${100-this.state.dividerTop}%`,
+      width:`${this.state.dividerLeft}%`
+
+    }
+
+    const renderer4Style={
+      left:`${this.state.dividerLeft}%`,
+      width:`${100-this.state.dividerLeft}%`,
+      top:`${this.state.dividerTop}%`,
+      height:`${100-this.state.dividerTop}%`,
+      
+    }
+
     return (
       <div className="App">
         <div className="App-toolbar">
@@ -19,11 +97,25 @@ class App extends Component {
           <div className="Button" onClick={e=>{this.onImportVolume(e)}}> Import Volume </div>
         </div>
   
-        <div className="App-renderer">
-          <div className="App-renderer1"/>
-          <div className="App-renderer2"/>
-          <div className="App-renderer3"/>
-          <div className="App-renderer4"/>
+        <div className="App-renderer"
+            ref="rendererContainer"
+            onMouseMove={e=>{this.onMouseMove(e)}}
+            onMouseUp={e=>{this.onMouseUp(e)}}>
+          <div className="App-renderer1"
+              style={renderer1Style}/>
+          <div className="App-renderer2"
+              style={renderer2Style}/>
+          <div className="App-renderer3"
+              style={renderer3Style}/>
+          <div className="App-renderer4"
+              style={renderer4Style}/>
+
+          <div className="Divider-horizontal"
+              style={dividerHStyle}
+              onMouseDown={e=>{this.onMouseDown(e)}}/>
+          <div className="Divider-vertical" 
+              style={dividerVStyle}
+              onMouseDown={e=>{this.onMouseDown(e)}}/>
         </div>
       
       </div>
